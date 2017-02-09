6bc8a9bc7250af5d3055bc1c986106fd4a1a0c9a
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2017-02-09 15:21:36 -0600

    Add files via upload

--- ListofProgramsALL.java
+++ ListofProgramsALL.java
@@ -16,7 +16,7 @@ public class ListofProgramsALL {
 	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-			String progid = "biology";
+			String progid = "math";
 			driver.manage().window().maximize();
 			
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -38,18 +38,24 @@ driver.findElement(By.xpath("//li[@class='currentlyShowing']")).click();
 Thread.sleep(1000);
 //print total programs
 String total = driver.findElement(By.xpath("//*[@id='site-content']/div/div/div[5]/div[1]/ul/li[2]")).getText();
-String h = total.substring(3, 6);
+
+
+
+String h= total.replaceAll("[^0-9]", "");
+
+
+//String h = total.substring(3, 6);
 int hi = Integer.parseInt(h);
 System.out.println(h);
-//get all the tags
-List<WebElement> lis=driver.findElements(By.xpath("//ul[@class='showAmount']/li/a"));
 
-System.out.println("Total pagers are "+lis.size()/2);
+
+
 Thread.sleep(1000);
+//selecti 1st element to get full url
 driver.findElement(By.xpath("//ul[@class='showAmount']/li[1]/a")).click();
 String url = driver.getCurrentUrl();
 String url2 = url.substring(0, url.length()-1);
-System.out.println(url2);
+System.out.println("total programs: "+url2);
 
 for (int i=0; i<hi; i=i+50) {
 	System.out.println(i);
@@ -57,9 +63,10 @@ for (int i=0; i<hi; i=i+50) {
 	
 	//print all disciplines
 	List<WebElement> links=driver.findElements(By.xpath("//li[starts-with(@id, 'srchProds')]/a"));
-	System.out.println("Total links are "+links.size());
+	System.out.println("Total links: "+links.size());
 	for(int u=0;u<links.size();u++)	{
 		WebElement ele= links.get(u);
+		
 		String pro=ele.getText();
 		System.out.println(u+1 + "  " +pro);		
 									}
