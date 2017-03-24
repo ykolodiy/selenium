190ad67a508729984a35cc76b8e4b3de731767a3
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2017-03-24 13:39:04 -0500

    Update PuttoPMDB.java

--- PuttoPMDB.java
+++ PuttoPMDB.java
@@ -8,6 +8,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PuttoPMDB {
@@ -18,7 +19,7 @@ public class PuttoPMDB {
 			
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			driver.get("http://k12cms./login.cfm");
+			driver.get("");
 			
 			driver.findElement(By.name("login")).sendKeys("");
 			driver.findElement(By.name("password")).sendKeys("");
@@ -26,7 +27,6 @@ public class PuttoPMDB {
 			
 			Thread.sleep(1200);
 			
-					
 					File src = new File ("C:\\Users\\vkoloyu\\pmdb.xlsx");
 					FileInputStream fis = new FileInputStream (src);
 					XSSFWorkbook wb = new XSSFWorkbook(fis);
@@ -38,35 +38,60 @@ public class PuttoPMDB {
 						driver.findElement(By.id("keyword")).sendKeys(data);
 			//click what found
 						driver.findElement(By.xpath("//ul[@role='listbox']/li/a")).click();
-						Thread.sleep(800);
+						Thread.sleep(1100);
 			//assets
 						driver.findElement(By.linkText("Assets")).click();
-						Thread.sleep(800);
-						if (!driver.findElement(By.linkText("Remove")).isDisplayed()){ 
+						Thread.sleep(2000);
 						
 						
-						//select file
-						driver.findElement(By.xpath("//input[@name='new_file_3']")).click();
-						Thread.sleep(800);
-						
-						String t = "C:\\Users\\vkoloyu\\alfrescoPDF\\03_10_2017\\50\\PNG\\";
-						//start autoit to insert url
-						
-						//Runtime.getRuntime().exec("C:\\Users\\vkoloyu\\autoit\\alfrescoAutoIt.exe");
-						
-						Runtime.getRuntime().exec("C:\\Users\\vkoloyu\\autoit\\alfrescoAutoIt2.exe"+" "+t+data+".png");
-						Thread.sleep(800);
-						//submit
-						//driver.findElement(By.xpath("//input[@name='submit_upload_file_3']")).click();
-						Thread.sleep(800);
-						} else {System.out.println("manually this isbn " +data);}
-						
+						WebElement r = driver.findElement(By.xpath(".//*[@id='content']/table/tbody/tr/td[2]/form/table[2]/tbody/tr[7]/td[2]"));
+						if (r.getText().contains("No file currently loaded.")){
 					
 						
+							
+						//select file
+						driver.findElement(By.xpath("//input[@name='new_file_3']")).click();
+						Thread.sleep(1100);
+						
+						String t = "C:\\Users\\vkoloyu\\alfrescoPDF\\03_17_2017\\50\\PNG\\";
+						//start autoit to insert url
+					
+						Runtime.getRuntime().exec("C:\\Users\\vkoloyu\\autoit\\alfrescoAutoIt2.exe"+" "+t+data+".png");
+						Thread.sleep(1100);
+						//submit
+						driver.findElement(By.xpath("//input[@name='submit_upload_file_3']")).click();
+						Thread.sleep(1100);
+						System.out.println(data);
+						}
+						
+						else {
+							driver.findElement(By.linkText("Remove")).click();
+							Thread.sleep(1100);
+							driver.navigate().refresh();
+							Thread.sleep(1100);
+						
+							driver.findElement(By.xpath("//input[@name='new_file_3']")).click();
+							Thread.sleep(1100);
+							
+							String t = "C:\\Users\\vkoloyu\\alfrescoPDF\\03_17_2017\\50\\PNG\\";
+							//start autoit to insert url
+						
+							Runtime.getRuntime().exec("C:\\Users\\vkoloyu\\autoit\\alfrescoAutoIt2.exe"+" "+t+data+".png");
+							Thread.sleep(1100);
+							//submit
+							driver.findElement(By.xpath("//input[@name='submit_upload_file_3']")).click();
+							Thread.sleep(1100);
+							System.out.println(data);
+						
+						
+						}
+						
+					
+					
 					}
 						
 				wb.close();		
-						
+				driver.close();		
 	}
 
 }
