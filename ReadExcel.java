4facb5782e9ed7f21b99c5284579c58fc45a202f
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2017-04-02 22:40:52 -0500

    Update ReadExcel.java

--- ReadExcel.java
+++ ReadExcel.java
@@ -1,3 +1,53 @@
+scripts custom function spreadsheet tool>script editor
+/**
+functionss1
+@customFunction
+*/
+
+function FUU1 (args){
+var total=0;
+  for (var i=0; i<args.length; i++)  {total = total + parseInt(args[i]); }
+  return total; }
+
+
+/**
+functionss2
+@customFunction
+*/
+
+function FUU2 (args){
+  if (args[2]>444) {
+    return parseInt(args[7])+parseInt(args[11]);}
+
+  else {return "args[2]<444"}
+
+
+}
+
+/**
+functionss3
+@customFunction
+*/
+
+function FUU3 (args){
+  var myObj = args[4];
+if(myObj=="17") {
+    return "seventeen";
+} else {
+    return 1;
+}
+
+
+}
+
+
+
+++++++++++++===========================================================================================
+
+
+
+
+
 package keyworddriven;
 
 import java.io.FileInputStream;
