c73f88ba808d272924c2cc6e79b324ab304efdae
Yuriy Kolodiy <yuriy.kolodiy@gmail.com>
2018-12-26 12:37:26 -0600

    Update ClickanyA.java

--- ClickanyA.java
+++ ClickanyA.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ClickanyA {
-
+ 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
